8e34bd530747e83a3d267f6306a95f6de5e0b4a2
MiguelGSilvestre <guegosilvestre67@gmail.com>
2021-05-17 10:18:57 -0300

    adicionando mais algumas linhas de codigo no programa bank

--- bank/lib/conta.ex
+++ bank/lib/conta.ex
@@ -1,11 +1,26 @@
 defmodule Conta do
 
   defstruct usuario: Usuario, saldo: 1000
+  @contas "contas.txt"
 
-  def cadastrar(usuario), do: %__MODULE__{usuario: usuario}
+  def cadastrar(usuario) do
+    contas = busca_contas()
+
+    binary = [%__MODULE__{usuario: usuario}] ++ busca_contas()
+    |> :erlang.term_to_binary()
+     File.write(@contas, binary)
+  end
+
+  def busca_contas do
+    {:ok, binary} = File.read(@contas)
+    :erlang.binary_to_term(binary)
+  end
+
+  def busca_contas_email(email), do: Enum.find(busca_contas(), &(&1.usuario.email == email))
+
   def transferir(contas, de, para, valor) do
-    de = Enum.find(contas, fn conta -> conta.usuario.email == de.usuario.email end)
-
+    #de = Enum.find(contas, fn conta -> conta.usuario.email == de.usuario.email end)
+    de = busca_contas_email(de.usuario.email)
     cond do
       valida_saldo(de.saldo, valor) -> {:error, "Saldo insuficiente!"}
       true ->
@@ -15,6 +30,7 @@ defmodule Conta do
         {de, para}
     end
   end
+
 
   def sacar(conta, valor) do
     cond do
